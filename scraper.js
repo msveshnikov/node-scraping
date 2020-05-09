90e67f5193c12d762b3b8774c01b22e28830eb36
Max Sveshnikov <msveshnikov@gmail.com>
2020-05-09 15:11:16 +0200

    bbc started

--- scraper.js
+++ scraper.js
@@ -1,7 +1,7 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const siteUrl = "https://remoteok.io/";
+const siteUrl = "https://bbc.com/";
 
 let siteName = "";
 const categories = new Set();
@@ -10,34 +10,34 @@ const locations = new Set();
 const positions = new Set();
 
 const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  return cheerio.load(result.data);
+    const result = await axios.get(siteUrl);
+    return cheerio.load(result.data);
 };
 
 const getResults = async () => {
-  const $ = await fetchData();
+    const $ = await fetchData();
 
-  siteName = $('.top > .action-post-job').text();
+    siteName = $(".module__title").text();
 
-  $(".tags .tag").each((index, element) => {
-    tags.add($(element).text());
-  });
-  $(".location").each((index, element) => {
-    locations.add($(element).text());
-  });
-  $("div.nav p").each((index, element) => {
-    categories.add($(element).text());
-  });
-  $('.company_and_position [itemprop="title"]').each((index, element) => {
-    positions.add($(element).text());
-  });
-  return {
-    positions: [...positions].sort(),
-    tags: [...tags].sort(),
-    locations: [...locations].sort(),
-    categories: [...categories].sort(),
-    siteName,
-  };
+    $(".tags .tag").each((index, element) => {
+        tags.add($(element).text());
+    });
+    $(".location").each((index, element) => {
+        locations.add($(element).text());
+    });
+    $("ul li[class^='orb-nav']").each((index, element) => {
+        categories.add($(element).text());
+    });
+    $('.company_and_position [itemprop="title"]').each((index, element) => {
+        positions.add($(element).text());
+    });
+    return {
+        positions: [...positions].sort(),
+        tags: [...tags].sort(),
+        locations: [...locations].sort(),
+        categories: [...categories].sort(),
+        siteName,
+    };
 };
 
 module.exports = getResults;
